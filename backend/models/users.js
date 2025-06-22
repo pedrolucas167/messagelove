227563ad67474194329e1ceeeed2ec1a9c382cda
pedrolucas167 <poloniosh@outlook.com>
2025-06-21 23:36:39 -0300

    refactor: restructure user model to use class-based definition and improve code organization.

--- backend/models/users.js
+++ backend/models/users.js
@@ -1,29 +1,46 @@
-const { DataTypes } = require('sequelize');
-const db = require('./index');
+// /backend/models/users.js - VERSÃO CORRIGIDA
 
-const User = db.sequelize.define('User', {
-  id: {
-    type: DataTypes.UUID,
-    defaultValue: DataTypes.UUIDV4,
-    primaryKey: true,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-    validate: { isEmail: true },
-  },
-  password: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-}, {
-  timestamps: true,
-  tableName: 'Users',
-});
+'use strict';
+const { Model } = require('sequelize');
 
-module.exports = User;
+// A mágica está aqui: exportamos uma função que recebe os argumentos do index.js
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    // Se você tiver associações, elas vêm aqui.
+    static associate(models) {
+      // Exemplo: User.hasMany(models.Card, { foreignKey: 'userId' });
+    }
+  }
+
+  // Usamos User.init para definir o modelo
+  User.init({
+    // Suas colunas exatamente como estavam:
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      primaryKey: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: { isEmail: true },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  }, {
+    // Suas opções exatamente como estavam:
+    sequelize, // Passamos a instância do sequelize aqui
+    modelName: 'User',
+    timestamps: true,
+    tableName: 'Users',
+  });
+
+  return User;
+};
