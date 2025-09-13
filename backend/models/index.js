b07bf84d31fea205e78f70112f5e0e8324dd15c7
pedrolucas167 <poloniosh@outlook.com>
2025-09-13 17:45:26 -0300

    refactor(index): remove commented file name and fix User model import

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,9 +1,8 @@
-// Arquivo: models/index.js
-
 'use strict';
 
 const { Sequelize, DataTypes } = require('sequelize');
 
+// Carrega a URL do banco de dados das variáveis de ambiente
 const databaseUrl = process.env.DATABASE_URL;
 if (!databaseUrl) {
   throw new Error("A variável de ambiente DATABASE_URL não foi definida.");
@@ -18,14 +17,16 @@ const sequelize = new Sequelize(databaseUrl, {
       rejectUnauthorized: false
     }
   },
-  logging: false, 
+  logging: false,
 });
 
+// Inicializa os modelos
 const db = {};
 
-db.User = require('./Users')(sequelize, DataTypes);
+db.User = require('./User')(sequelize, DataTypes);
 db.Card = require('./card')(sequelize, DataTypes);
 
+// Define associações
 if (db.User.associate) db.User.associate(db);
 if (db.Card.associate) db.Card.associate(db);
 
